79640dde7be1fa86a1b7c289aa045f47aa031886
Felixburton7 <felixburton2002@gmail.com>
2024-05-15 12:42:55 -0700

    Added some styling

--- lib/posts/bloc/widgets/post_list_item.dart
+++ lib/posts/bloc/widgets/post_list_item.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_infinite_list/posts/posts.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:flutter_infinite_list/posts/posts.dart';
 import 'package:http/http.dart' as http;
 import '../bloc/post_bloc.dart';
 import '../models/post.dart';
@@ -10,7 +7,7 @@ import '../widgets/bottom_loader.dart';
 import '../widgets/post_list_item.dart';
 
 class PostListItem extends StatelessWidget {
-  const PostListItem({required this.post, super.key});
+  const PostListItem({required this.post, Key? key}) : super(key: key);
 
   final Post post;
 
@@ -18,12 +15,39 @@ class PostListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return Material(
-      child: ListTile(
-        leading: Text('${post.id}', style: textTheme.bodySmall),
-        title: Text(post.title),
-        isThreeLine: true,
-        subtitle: Text(post.body),
-        dense: true,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 5),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.topRight,
+              colors: [
+                Color(0xFF2193b0), // Start color: #2193b0
+                Color(0xFF6dd5ed), // End color: #6dd5ed
+              ],
+            ),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Color.fromARGB(0, 11, 11, 11)),
+          ),
+          child: ListTile(
+            leading: Text('${post.id}',
+                style: TextStyle(
+                    color: Colors.white)), // Update text color to white
+            title: Text(
+              post.title,
+              style: TextStyle(
+                color: Colors.black, // Update text color to white
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            isThreeLine: true,
+            subtitle: Text(post.body,
+                style: TextStyle(
+                    color: Colors.white)), // Update text color to white
+            dense: true,
+          ),
+        ),
       ),
     );
   }
